e4bc6067ba14ff1c9567d154e1ffaaafc5c71908
Jonifwun <jonathanphilipday@gmail.com>
2020-07-12 10:51:14 -0400

    add input pre-fill for register page errors

--- controllers/index.js
+++ controllers/index.js
@@ -5,26 +5,35 @@ module.exports = {
 
     //GET /register
     getRegister(req, res, next){
-        res.render('register', {title: 'Register'})
+        res.render('register', {title: 'Register', username: '', email: '', location: '', department: ''})
     },
     //POST /register
     async postRegister(req, res, next){
-    //EDIT THIS ACCORDING TO IANS COURSE (124);    
-
-    const newChemist = new Chemist ({
-        username: req.body.username,
-        location: req.body.location,
-        department: req.body.department,
-        email: req.body.email 
-        });
    
-        let chemist = await Chemist.register(newChemist, req.body.password);
-        
-        req.login(chemist, function(err){
-            if(err) return next(err);
-            req.session.success = `Welcome, ${chemist.username}`;
-            res.redirect("/");
-        })        
+    try {
+        const newChemist = new Chemist ({
+            username: req.body.username,
+            location: req.body.location,
+            department: req.body.department,
+            email: req.body.email 
+            });
+       
+            const chemist = await Chemist.register(newChemist, req.body.password);
+            
+            req.login(chemist, function(err){
+                if(err) return next(err);
+                req.session.success = `Welcome, ${chemist.username}`;
+                res.redirect("/");
+            })        
+    } catch(err) {
+        const { username, email, location, department } = req.body;
+        let error = err.message;
+        if(error.includes('duplicate key') && error.includes('index: email_1 dup key')){
+            error = "A user with the given email is already registered"
+        }
+        res.render('register', {title: 'Register', username, email, location, department, error})
+    }    
+    
        
     },
     getLogin(req, res, next){
