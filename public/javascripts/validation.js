5280f293c59e0df3c7b4d0844a255f054189a7a4
Jonifwun <jonathanphilipday@gmail.com>
2020-07-16 20:53:27 -0400

    add email validation

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,22 +1,42 @@
-const form = document.querySelector("form");
+const form = document.querySelector("#register-form");
 const newPassword = document.querySelector("#new-password")
 const confirmation = document.querySelector("#new-password-confirm")
-const UImessage = document.querySelector("#validation");
+const UImessagePass = document.querySelector("#pass-validation");
+const UImessageEmail = document.querySelector("#email-validation")
+const email = document.querySelector("#email")
 
-function validation(message, add, remove){
-    UImessage.textContent = message;
-    UImessage.classList.add(add);
-    UImessage.classList.remove(remove);
+function validatePassword(message, add, remove){
+    UImessagePass.textContent = message;
+    UImessagePass.classList.add(add);
+    UImessagePass.classList.remove(remove);
 }
+
+function validateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+       return true
+     } else {
+       return false
+     }
+   }
 
 confirmation.addEventListener('keyup', event => {
     if(newPassword.value !== confirmation.value){
-        validation('Passwords must match', 'matched', 'unmatched')
+        validatePassword('Passwords must match', 'matched', 'unmatched')
     } else {
-        validation('Passwords match', 'unmatched', 'matched')
+        validatePasswords('Passwords match', 'unmatched', 'matched')
     }
     if (confirmation.value === ''){
-        UImessage.textContent = "";
+        UImessagePass.textContent = "";
     }    
 });
+
+
+
+email.addEventListener('keyup', event => {
+    if(!validateEmail(email.value)){
+        UImessageEmail.textContent = 'Please enter a valid email address';
+    } else {
+        UImessageEmail.textContent = '';
+    }
+})
 
